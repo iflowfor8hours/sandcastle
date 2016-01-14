8791293a534aa11fa03bfa30dc8f0025f1425930
Vladimir Zelmanov <vzelmanov@gmail.com>
2016-01-13 22:07:32 -0800

    Import gpg keys for backups

--- test/gen-duplicity-keys.sh
+++ test/gen-duplicity-keys.sh
@@ -8,6 +8,10 @@ else
   OUR_GPG=gpg
 fi
 
+extract_key_id() {
+  head -1 | cut -d/ -f2 | cut -d\  -f1
+}
+
 generate_keypair() {
   TARGET="$1"
 
@@ -28,7 +32,17 @@ EOGPG
 
   $OUR_GPG --homedir "$GPG_HOME" --export --armor Squidy > "$TARGET-public.key"
   $OUR_GPG --homedir "$GPG_HOME" --export-secret-keys --armor Squidy > "$TARGET-private.key"
+
+  $OUR_GPG --homedir "$GPG_HOME" --list-keys --armor Squidy | extract_key_id > "$TARGET-public.key-id"
+  $OUR_GPG --homedir "$GPG_HOME" --list-secret-keys --armor Squidy | extract_key_id > "$TARGET-private.key-id"
 }
 
 generate_keypair $(dirname $0)/backup-encryption
 generate_keypair $(dirname $0)/backup-signing
+
+cat <<EOVARS > $(dirname $0)/vars.yml
+---
+backup_encryption_key_id: $(< $(dirname $0)/backup-encryption-public.key-id)
+backup_signing_key_id: $(< $(dirname $0)/backup-signing-private.key-id)
+
+EOVARS
